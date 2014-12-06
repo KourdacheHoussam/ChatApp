ddddd9e9ca5603b36ac9c349f661fd6b86c6282d
HoussamKourdache <kourdache88@yahoo.fr>
2014-12-06 16:06:30 +0100

    server

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,29 +1,27 @@
 /**
  * Created by Housssam on 05/12/2014.
- */
-console.log("salut");
-
-/**
+ *
  * Tout d'abord on va inclure les librairies
  * que l'on souhaite utiliser à l'aide de la
  * méthode require()
  */
 
-var http, httpServer;
+var http, httpServer, socket_io;
 
 http = require('http');
+socket_io=require('../../libs/node_modules/socket.io');
 
 /**
  * Dans ce qui suit , on va créer un serveur, qui prends une fonction en paramètre
- * recevant la requête et la réponse à renvoyer à l'utilisateur
+ * recevant la requête envoyée et la réponse à renvoyer à l'utilisateur
  */
-
 httpServer=http.createServer(function(req, rep){
     rep.end("hello");
+    console.log("hello");
 });
 
 /**
  * Maintenant faut se connecter à ce serveur en écoutant un port
  */
-
-httpServer.listen(1337);
+var port=2222;
+httpServer.listen(port);
