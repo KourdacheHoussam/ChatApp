e4bb9f144b3e685bd5a95df35343c83a1cd4eddb
HoussamKourdache <kourdache88@yahoo.fr>
2014-12-06 21:09:17 +0100

    server

--- backend/client/client.js
+++ backend/client/client.js
@@ -19,6 +19,30 @@
     /** io utilisé ci-dessous est chargé automatiquement via le JS intégré dans index.html
     * on appelle la méthode connect du composant io(chargé de socket) pour nous connecter au serveur.*/
 
-    var socket = io.connect('http://localhost:2222');
+    var socket = io.connect('http://localhost:1337');
+    console.log(io);
+
+    /** A l'aide de jquery on va recupérer les elements pseudo et email de l'user **/
+
+
+    $('#loginform').submit(function(event){
+        event.preventDefault(); //empecher l'utilisateur de soumettre le formulaire
+
+        /**
+         * puis émettre un évènement à la socket
+         * on spécifiant le nom de l'évènement que l'on veut
+         */
+
+        /**
+         * Notre premiere evenement est le : LOGIN : envoi d'un event login au serveur
+         */
+
+        socket.emit('login', {  // on emet un event de type loginEvent + les paramètres (pseudo+mail)
+            pseudo:$('#pseudo').val(),  // on récupere la valeur du cham pseudo, puis
+            adressemail:$('#adressemail').val() // celle du champ adressemail
+        });
+
+        socket.emit('try', { val:"cocou"});
+    });
 
 })(jQuery);
--- backend/server/server.js
+++ backend/server/server.js
@@ -8,10 +8,10 @@
 
 //Variables globales stockés sur le serveur js utilisables par tous les clients
 var http, httpServer, io, md5;
-var port=2222;
+var port=1337;
 
 http = require('http');  /** Serveur http de Node.JS **/
-md5 = require("md5");
+md5 = require('md5');
 
 /**
  * Dans ce qui suit , on va créer un serveur, qui prends une fonction en paramètre
@@ -23,27 +23,33 @@ httpServer=http.createServer(function(req, rep){
     console.log("Un utilisateur a affiché la page html");
 });
 
-/** Maintenant faut se connecter à ce serveur en écoutant un port */
-httpServer.listen(port);
-
 
 /** maintenant on va écouter les connections/communication qui se font
  * via notre serveur node.js à l'aide de la socket socket_io */
 
 io=require('socket.io').listen(httpServer); /** Socket.IO pour utiliser des sockets**/
 
+/** Maintenant faut se connecà ce serveur en écoutant un port */
+httpServer.listen(port);
 
 /**
- *
  *  Il est temps maintenant d'écouter les connections: dés qu'il y a une connection
  * sur une socket on lance la fonction anonyme qui prend la socket de l'utilisateur en cours
- *
  **/
 
-/**
- *  on écoute l'evènement "connection"
- */
+/**  on écoute l'evènement "connection" */
 io.sockets.on('connection', function(socket){    /** socket = socket utilisateur en cours*/
     console.log("nouveau utilisateur");
+    /** Dans cette partie on reçoit les évènement prevenant du côté client */
+    socket.on('login', function(user){  // A la réception de loginEvent, j'envoi une fonction de CallBack
+        console.log(user.adressemail);
+    });
 
+
+
+    socket.on('try', function(msg){
+        console.log(msg) ;
+    });
 });
+
+
