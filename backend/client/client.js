5d1d2bb93b6a45c476713a34ebee1d63cea0f1e7
HoussamKourdache <kourdache88@yahoo.fr>
2014-12-07 18:51:05 +0100

    up

--- backend/client/client.js
+++ backend/client/client.js
@@ -1,42 +1,37 @@
 /**
- * Created by Housssam on 06/12/2014.
- */
-/**
- * Ici, on va se connecter au site depuis SOCKET.IO
- * Remarque : ici on utilise JQUERY
- *
- *  --1--  : connexion à socket IO
- */
+* Created by Housssam on 06/12/2014.
+* Ici, on va se connecter au site depuis SOCKET.IO
+* Remarque : ici on utilise JQUERY
+*/
 
-(function($){
+(function($) {
 
     /** io utilisé ci-dessous est chargé automatiquement via le JS intégré dans index.html
-    * on appelle la méthode connect du composant io(chargé de socket) pour nous connecter au serveur.*/
+     * on appelle la méthode connect du composant io(chargé de socket) pour nous connecter au serveur.*/
+    var socket=io('http://localhost:3000/');
 
-    var socket = io.connect('http://localhost:1337');
-
-    /** A l'aide de jquery on va recupérer les elements pseudo et email de l'user **/
-
-
-    $('#login-form').submit(function(event){
-
+    /** A l'aide de jquery on va recupérer les elements "username" et "email" de l'utilisateur **/
+    $('#loginform').submit(function(event){
+        console.log("inside submit method #client");
         event.preventDefault(); //empecher l'utilisateur de soumettre le formulaire
-
         /**
          * puis émettre un évènement à la socket
          * on spécifiant le nom de l'évènement que l'on veut
-         */
-
-        /**
          * Notre premiere evenement est le : LOGIN : envoi d'un event login au serveur
          */
-
-        socket.emit('login', {  // on emet un event de type loginEvent + les paramètres (pseudo+mail)
-            pseudo:$('#pseudo').val(),  // on récupere la valeur du cham pseudo, puis
-            email:$('#email').val() // celle du champ adressemail
-        });
-
-
+        socket.emit('login', {
+                username : $('#username').val(),
+                mail 	 : $('#mail').val()
+            })
     });
 
-})(jQuery);
+    /** Vérifier que l'user est bien crée on écoutant l'evement provenant du serveur (new-user-created)*/
+    socket.on('new-user-created', function(created_user){
+        alert("new user created !");
+        $('#liste-utilisateurs').append('<li> <img src="' + created_user.avatar + '"</li>');
+    });
+    console.log("after submit method #client");
+
+
+    }//END ANONYME FUNCTION
+ )(jQuery);//END SCRIPT
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,53 +1,54 @@
 /**
  * Created by Housssam on 05/12/2014.
- *
- * Tout d'abord on va inclure les librairies
- * que l'on souhaite utiliser à l'aide de la
- * méthode require()
- */
+ * Tout d'abord on va inclure les librairies que l'on souhaite utiliser à l'aide de la
+ * méthode require()*/
 
-//Variables globales stockés sur le serveur js utilisables par tous les clients
-var http, httpServer, io, md5;
+ //Variables globales stockés sur le serveur js utilisables par tous les clients
 var port=1337;
+var md5 = require('md5');
+var io, app, http_server, path, express;
+express=require('express');
+path=require('path');
+app=require('express')();
 
-http = require('http');  /** Serveur http de Node.JS **/
-md5 = require('md5');
+/** Dans ce qui suit , on va créer un serveur, qui prends une fonction en paramètre
+ * recevant la requête envoyée et la réponse à renvoyer à l'utilisateur*/
+http_server=require('http').Server(app);
 
-/**
- * Dans ce qui suit , on va créer un serveur, qui prends une fonction en paramètre
- * recevant la requête envoyée et la réponse à renvoyer à l'utilisateur
- */
 
-httpServer=http.createServer(function(req, rep){
-    rep.end("hello");
-    console.log("Un utilisateur a affiché la page html");
+io=require('socket.io')(http_server);
+
+app.get('/', function(req, rep){
+    rep.sendFile(path.resolve('../../index.html'));
 });
-
 
 /** maintenant on va écouter les connections/communication qui se font
  * via notre serveur node.js à l'aide de la socket socket_io */
 
-io=require('socket.io').listen(httpServer); /** Socket.IO pour utiliser des sockets**/
-
-/** Maintenant faut se connecà ce serveur en écoutant un port */
-httpServer.listen(port);
-
-/**
- *  Il est temps maintenant d'écouter les connections: dés qu'il y a une connection
- * sur une socket on lance la fonction anonyme qui prend la socket de l'utilisateur en cours
- **/
-
-/**  on écoute l'evènement "connection" */
 io.sockets.on('connection', function(socket){    /** socket = socket utilisateur en cours*/
-
-    console.log("nouveau utilisateur");
-
+    var current_user;
+    console.log("New User connected");
     /** Dans cette partie on reçoit les évènement prevenant du côté client */
     socket.on('login', function(user){  // A la réception de loginEvent, j'envoi une fonction de CallBack
+        /**  enregistrer les proprietes du client*/
+        current_user=user;
+        current_user.id=user.mail.replace('@', '-').replace('.', '-');
+        current_user.avatar='https://gravatar.com/avatar/'+ md5.digest_s(user.mail)+"?s=40";
+        /** prévenir l'utilisateur et tous les autrees que le new client est enregistre*/
+        socket.emit("new-user-created", current_user);
+        socket.broadcast.emit("new-user-created", current_user);
         console.log(user);
     });
 
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
 
+
+/** ecoute des sockets sur le port 3000 du serveur */
+http_server.listen(3000, function(){
+    console.log("Listening on : 3000");
 });
 
 
