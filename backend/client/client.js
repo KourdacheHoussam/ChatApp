dd17e88f2a546668b7a54bca2a0ba6acc46006c4
HoussamKourdache <kourdache88@yahoo.fr>
2014-12-08 11:42:39 +0100

    updated

--- backend/client/client.js
+++ backend/client/client.js
@@ -10,6 +10,42 @@
      * on appelle la méthode connect du composant io(chargé de socket) pour nous connecter au serveur.*/
     var socket=io('http://localhost:3000/');
 
+
+     /**
+     * -----------------------------------------------------------------------------------------------------------
+     * ------------------------------------------------------ GESTION MESSAGERIE---------------------------------
+     * -----------------------------------------------------------------------------------------------------------
+     */
+
+     var message_template=$('#message_tpl').html(); //recuperer la div où s'affiche les msg
+     $('#message_tpl').remove();//puis la supprimer
+
+     /** Quand j'envoie un message*/
+     $('#send-msg-form').submit(function(event){
+        event.preventDefault();
+        /** Prevenir le serveur qu'il y a un nouveau msg*/
+        socket.emit('new-message-coming', {
+           message : $('#message_content').val()
+        });
+        $('#message_content').val('');
+        $('#message_content').focus();
+     });
+
+     /** Quand je recois un message*/
+     socket.on('new-message-coming', function(message){
+         //on injecte le contenu message dans le template message_tmpl
+         $('#listmessages').append('<div class="message">' + Mustache.render(message_template, message) + '</div>');
+         $('#listmessages').animate({scrollTop: $('#message_tpl').offset().top }, 1000);
+     });
+
+
+
+    /**
+     * -----------------------------------------------------------------------------------------------------------
+     * ------------------------------------------------------ GESTION CONNECTIONS---------------------------------
+     * -----------------------------------------------------------------------------------------------------------
+     */
+
     /** A l'aide de jquery on va recupérer les elements "username" et "email" de l'utilisateur **/
     $('#loginform').submit(function(event){
         console.log("inside submit method #client");
@@ -22,16 +58,37 @@
         socket.emit('login', {
                 username : $('#username').val(),
                 mail 	 : $('#mail').val()
-            })
+        })
+        //on vide les champs
+        $('#username').val('');
+        $('#username').focus();
+        $('#mail').val('');
+        $('#mail').focus();
     });
+
+    /** Vérifier si le login est bien ok **/
+    socket.on('logging-ok', function(){
+        //window.location='http://localhost/projects/Chat/chatroom.html';
+        //$('#connect').style.visibility="hidden";
+        $('#messagerie').style.visibility="visible";
+        $('#message').focus();
+    });
+
 
     /** Vérifier que l'user est bien crée on écoutant l'evement provenant du serveur (new-user-created)*/
     socket.on('new-user-created', function(created_user){
-        alert("new user created !");
-        $('#liste-utilisateurs').append('<li> <img src="' + created_user.avatar + '"</li>');
+        $('#liste-utilisateurs').append('<img src="' + created_user.avatar + '" id="' + created_user.id + '">');
+        $('#liste-utilisateurs').append('<br/>');
     });
-    console.log("after submit method #client");
 
 
-    }//END ANONYME FUNCTION
+    /** on regarde qui se déconnecte */
+    socket.on('user-disconnected', function(user){
+        $("#" + user.id).remove();
+    });
+
+
+
+
+    }
  )(jQuery);//END SCRIPT
--- backend/server/server.js
+++ backend/server/server.js
@@ -11,13 +11,14 @@ express=require('express');
 path=require('path');
 app=require('express')();
 
+var list_users={}; //user list container
+var list_messages=[]; //cache message
+var history_limit=30; //limit taille list_messages
+
 /** Dans ce qui suit , on va créer un serveur, qui prends une fonction en paramètre
  * recevant la requête envoyée et la réponse à renvoyer à l'utilisateur*/
 http_server=require('http').Server(app);
-
-
 io=require('socket.io')(http_server);
-
 app.get('/', function(req, rep){
     rep.sendFile(path.resolve('../../index.html'));
 });
@@ -26,25 +27,73 @@ app.get('/', function(req, rep){
  * via notre serveur node.js à l'aide de la socket socket_io */
 
 io.sockets.on('connection', function(socket){    /** socket = socket utilisateur en cours*/
-    var current_user;
-    console.log("New User connected");
-    /** Dans cette partie on reçoit les évènement prevenant du côté client */
+    var current_user=false;
+
+    /** afficher tout les utilisateurs dans la liste*/
+    for(var user in list_users){
+        socket.emit('new-user-created', list_users[user]);
+    }
+    for(var msg in list_messages){
+        socket.emit('new-message-coming', list_messages[msg]);
+    }
+
+    /**
+     * ---------------------------------------------------------------------
+     * ---------------- GESTION MESSAGERIE---------------------------------
+     * ---------------------------------------------------------------------
+     */
+
+    /** Message reçu */
+    socket.on('new-message-coming', function(message){
+        message.user=current_user;
+        var date=new Date();
+        message.hour=date.getHours();
+        message.minutes=date.getMinutes();
+        //sauvegarder le message
+        list_messages.push(message);
+        if(list_messages.length>history_limit){
+            list_messages.shift();//enlever le premier element du tableau
+        }
+        //Renvoyer le message à tous les utilisateurs
+        io.sockets.emit('new-message-coming', message);
+    });
+
+    /**
+     * ---------------------------------------------------------------------
+     * ---------------- GESTION CONNECTIONS---------------------------------
+     * ---------------------------------------------------------------------
+     */
+
+    /** ---LOGIN---- Dans cette partie on reçoit les évènement prevenant du côté client  **/
     socket.on('login', function(user){  // A la réception de loginEvent, j'envoi une fonction de CallBack
         /**  enregistrer les proprietes du client*/
         current_user=user;
         current_user.id=user.mail.replace('@', '-').replace('.', '-');
-        current_user.avatar='https://gravatar.com/avatar/'+ md5.digest_s(user.mail)+"?s=40";
+        current_user.avatar='http://robohash.org/'+  md5.digest_s(user.mail) +'/arfset_bg1/3.14159?size=60x60';
+
         /** prévenir l'utilisateur et tous les autrees que le new client est enregistre*/
-        socket.emit("new-user-created", current_user);
-        socket.broadcast.emit("new-user-created", current_user);
+        io.sockets.emit("new-user-created", current_user);
+
+        /** ajouter le nouveau dans la liste */
+        list_users[current_user.id]=current_user;
+        console.log(list_users);
+
+        /** lui dire qu'il est bien connecté **/
+        socket.emit("logging-ok");
         console.log(user);
     });
 
+    /**
+     * ----QUIT---- Quand l'user quitte le chat, on le supprime de la liste
+     */
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        if(!current_user){   return false;  }
+        delete list_users[current_user.id];
+        //prevenir les autres users
+        io.sockets.emit('user-disconnected', current_user);
     });
-});
 
+});
 
 /** ecoute des sockets sur le port 3000 du serveur */
 http_server.listen(3000, function(){
