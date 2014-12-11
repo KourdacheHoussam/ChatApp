b54f937f847bbb68f2414bfd2f1ba90c070f6585
HoussamKourdache <kourdache88@yahoo.fr>
2014-12-11 09:30:59 +0100

    up

--- backend/client/client.js
+++ backend/client/client.js
@@ -4,12 +4,22 @@
 * Remarque : ici on utilise JQUERY
 */
 
-/** Document Pret*/
+/**-------------------
+ * ---Document Pret --
+ * ------------------*/
 (function($) {
 
-
-
+    /**----------------------------------------------------------------------------------------------------------
+     *  -------------------------------------- VARIABLES  --------------------------------------------------------
+     * ----------------------------------------------------------------------------------------------------------- */
+    var list_messages=[];
+    var mode_actuel='';
+    var date_connection=new Date();
+    var date_hour=date_connection.getHours();
+    var date_minutes=date_connection.getMinutes();
+    var date_seconds=date_connection.getSeconds();
     var mode_communication='', voir_anciens_msg='', button_quitter=false;
+
     $('#send-msg-form').on('change', function(){
         // Mode Communication Var
         mode_communication= jQuery( 'input[name="modecom"]:checked' ).val();
@@ -23,18 +33,177 @@
         alert(button_quitter);
     });
 
+    /** ----------------------------------------------------------------------------------------------------------
+     *  -------------------------------------- FONCTIONS COMMUNES ------------------------------------------------
+     *  ---------------------------------------------------------------------------------------------------------- */
+
+    // fonction pour prévenir le serveur du choix de mode de COM
+    function chooseModeCommunication(mode){
+        $.ajax({
+            url:'http://localhost:1337/choosemode/',
+            type:'GET',
+            data: 'modecommunication=' + mode,
+            dataType:'json',
+            complete: function(data, status, xhr){ //recevoir une confirmation du serveur
+                //si c bon, on coche la case du mode choisi sur l'IHM
+                // on démarre le long polling()
+                alert("La communication avec ce mode est établie");
+                mode_actuel=JSON.stringify(data);
+                alert("Mode actuel =" +mode_actuel);
+            },
+            fail: function(xhr, status){
+                alert("La communication avec ce mode n'est pas établie");
+            }
+        });
+
+    };
+    //fonction pour envoyer un nouveau message
+    function sendNewMessage(message){
+        $.ajax({		// la méthode ajax() est utilisé pour effectuer une requête réseau asynchrone.
+            url: 'http://localhost:1337/addmessage/',	// l'url vers laquelle j'envoie ma requete
+            type: 'GET',				// specifie le type de requete GET ou POST
+            data: 'message=' + message, //specifie les données à envoyer au serveur
+            dataType: 'json',			//le type de données attendue par le serveur, ici c du json
+            //success est la méthode à faire tourner quand la requete aboutit
+            complete: function(data, status, xhr){
+                if(status="success"){
+                    var msg='';
+                    //var obj=$.parse(data);
+                    list_messages=JSON.stringify(data);
+                }
+            },
+            fail:function(xhr, status){
+                alert("il y a une error");
+            }
+            // la méthode à appeler quand la requete est finie: quand on a reçu le callback success ou error.
+            //complete:
+        });
+        return false; //empecher le rafraichissement
+    };
+
+    /** ----------------------------------------------------------------------------------------------------------
+     *  -------------------------------------- GESTION DES BOUTONS -----------------------------------------------
+     *  ---------------------------------------------------------------------------------------------------------- */
+
+    //Quand on choisit un mode
+    $('#choix-mode').submit( function(){
+        // on récupère le contenu de l'input
+        var mode = $('#choix-mode input[name=mode]').val();
+        chooseModeCommunication(mode);
+        return false;//empecher le rafraichissement
+    });
+
+    // quand le formulaire est envoyé
+    $('#send-message').submit( function(){
+        // on récupère le contenu de l'input
+        var message = $('#send-message input[name=message]').val();
+        // Si le contenu du message du formulaire est null
+        if( jQuery.trim( message ) == '' ){
+            window.alert('Enter a message!');
+            return false;
+        }
+        sendNewMessage(message);//send it
+        return false;//empecher le rafraichissement
+    });
+
+
+    /** ----------------------------------------------------------------------------------------------------------------
+     ---------------------------------------------- SECTION POLLING ----------------------------------------------------
+     -------------------------------------------------------------------------------------------------------------------*/
+
+    function polling(){
+        var time;
+        $.ajax({
+            url:'http://localhost:1337/polling/',
+            type:'GET',
+            data: { heure: date_hour, minutes:date_minutes, secondes:date_seconds },
+            dataType:'json',
+            success: function(data, status, xhr){
+                clearInterval(time);
+                if(status="success"){
+                    var msg='';
+                    list_messages=JSON.stringify(data);
+                    $('#message-sent').append("<p>Votre message est reçu: contenu  " + list_messages.toString()+"</p>");
+                    time=setTimeout( function(){
+                        polling();
+                    }, 2000 );  // redemarre le polling au bout de 2 sec
+                }else{
+                    alert(':( Please refresh the page!');
+                }
+            },
+            error:function(){
+                clearInterval( time );
+                time = setTimeout( function(){
+                    polling( );
+                }, 19000 );//on attend 15 scd
+            }
+        });
+        return false;
+    };
+    // Appel à la fonction polling
+    //polling();
+
+    /** -----------------------------------------------------------------------------------------------------------------
+     ------------------------------------------------ SECTION LONG POLLING ----------------------------------------------
+     --------------------------------------------------------------------------------------------------------------------*/
+
+     // Start Long-polling for messages
+     // on crée une novuelle fonction javascript
+     function longPolling(  ){
+        var time;
+        //on envoie la requete vers le serveur dont le port est 1337:
+        //on lui passe en parametre un timestamp et un lastid
+        jQuery.ajax({
+            url: 'http://localhost:1337/longpolling/',
+            type: 'GET',
+            data: { heure: date_hour, minutes:date_minutes, secondes:date_seconds },
+            dataType: 'json',
+            //SUCESS et ERROR sont deux call back renvoyées par le serveur
+            success: function( data, status, xhr ){  //Au cas du succes on verifie si on a des resultats
+                clearInterval( time );
+                if( status == 'success' ){
+                    // quand on reçoit des données, on refait une nouvelle requete
+                    // aprés une seconde, en appelant
+                    // la fonction longpolling
+                    time=setTimeout( function(){
+                        longPolling( );
+                    }, 1000 );
+                    var msg='';
+                    list_messages=JSON.stringify(data);
+                    $('#message-sent').append("<p>Votre message est reçu: contenu  " + list_messages.toString()+"</p>");
+                }
+                else if( data.status == 'error' ){
+                    alert('We got confused, Please refresh the page!');
+                }
+            },
+            // Si la requete envoye renvoie une errer: exemple d'un serveur en panne
+            // ou pas de connexion internet.
+            // nous allons renvoyer une requete au bout de 15 seconde afin de laisse le temps au serveur de redemarrer
+            error: function(){
+                clearInterval( time );
+                time = setTimeout( function(){
+                    longPolling( );
+                }, 15000); //15 secondes
+            }
+            // quand la requete est complete
+            //complete:longPolling
+        });
+        return false;
+     }
+     //longPolling();
+
+    /** --------------------------------------------------------------------------------------------------------------------
+    *   -------------------------------------------------------- SECTION PUSH ----------------------------------------------
+    *   --------------------------------------------------------------------------------------------------------------------*/
+
+
 
     /** io utilisé ci-dessous est chargé automatiquement via le JS intégré dans index.html
      * on appelle la méthode connect du composant io(chargé de socket) pour nous connecter au serveur.*/
     var socket=io('http://localhost:3000/');
-
     var Me;
 
-     /**
-     * -----------------------------------------------------------------------------------------------------------
-     * ------------------------------------------------------ GESTION MESSAGERIE---------------------------------
-     * -----------------------------------------------------------------------------------------------------------
-     */
+     /**----------------------------------------------------- GESTION MESSAGERIE--------------------------------- */
 
      var message_template=$('#message_tpl').html(); //recuperer la div où s'affiche les msg
      $('#message_tpl').remove();//puis la supprimer
@@ -62,20 +231,14 @@
                  '</div>');
          }
          if(message.user.id == Me.id){
-             $('#listmessages').append('<div class="message-left" style="margin-bottom:-45px; ">'
+             $('#listmessages').append('<div class="message-left" style="margin-bottom:-45px" id="'+message.user.id+'">'
                  + Mustache.render(message_template_left, message) + '</div>');
          }
-         $('#listmessages-right').animate({scrollTop: $('#message_tpl').offset().top }, 1000);
-         $('#listmessages-right_left').animate({scrollTop: $('#message_tpl_right').offset().top }, 1000);
+         $('#messagerie').animate({scrollTop: $('.message-left').last().offset().top }, 1000);
+         //$('#listmessages-right_left').animate({scrollTop: $('#message_tpl_right').offset().top }, 1000);
      });
 
-
-
-    /**
-     * -----------------------------------------------------------------------------------------------------------
-     * ------------------------------------------------------ GESTION CONNECTIONS---------------------------------
-     * -----------------------------------------------------------------------------------------------------------
-     */
+    /** ------------------------------------------------------ GESTION CONNECTIONS--------------------------------- */
 
     /** A l'aide de jquery on va recupérer les elements "username" et "email" de l'utilisateur **/
     $('#loginform').submit(function(event){
@@ -129,15 +292,15 @@
 
 
 
-    }
- )(jQuery);//END SCRIPT
+    } //ENF OF MAIN FUNCTION
+ )(jQuery); //END SCRIPT
 
 
 function goToByScroll(id){
-    // Reove "link" from the ID
+    // Remove "link" from the ID
     id = id.replace("link", "");
     // Scroll
     $('html,body').animate({
-            scrollTop: $("#"+id).offset().top},
+            scrollTop: $("#"+id).first().offset().top},
         'fast');
 }
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,35 +1,176 @@
 /**
  * Created by Housssam on 05/12/2014.
- * Tout d'abord on va inclure les librairies que l'on souhaite utiliser à l'aide de la
- * méthode require()*/
+ * Tout d'abord on va inclure les librairies que
+ * l'on souhaite utiliser à l'aide de la méthode require()*/
 
- //Variables globales stockés sur le serveur js utilisables par tous les clients
-var port=1337;
-var md5 = require('md5');
-var io, app, http_server, path, express;
+/** ----------------------------------------------------------------------------------------------------------
+ *  ------------------------------ VARIABLES  & CREATION DU SERVEUR NODE(EXPRESS------------------------------
+ *  ---------------------------------------------------------------------------------------------------------- */
+
+var port=3000;  //Port de COM
+var md5 = require('md5');   //
+var io, app, http_server, path, express, util, url, date, queryString;
 express=require('express');
 path=require('path');
 app=require('express')();
-
+util=require('util');
+url=require('url');
+queryString=require('querystring');
 var list_users={}; //user list container
 var list_messages=[]; //cache message
 var history_limit=30; //limit taille list_messages
 var date;
-/** Dans ce qui suit , on va créer un serveur, qui prends une fonction en paramètre
- * recevant la requête envoyée et la réponse à renvoyer à l'utilisateur*/
+var clients_en_attente=[];
+//Créer un serveur avec une fonction en param  recevant la
+//requête envoyée et la réponse à renvoyer à l'utilisateur
 http_server=require('http').Server(app);
-
 io=require('socket.io')(http_server);
-app.get('/', function(req, rep){
-    rep.sendFile(path.resolve('../../index.html'));
+
+/** ---------------------------------------------------------------------------------------------------------
+ * ---------------------- QUAND ON APPELE URL(/addmessage) : localhost:port/addmessage ----------------------
+ * ----------------------------------------------------------------------------------------------------------*/
+ // Reception d'un nouveau message à enregistrer
+ app.get('/addmessage', function(req, rep){
+        console.log("Je suis dans addmessage")
+        var msg=req.query.message;
+        /** save message*/
+        if(req.query.message != null){
+            saveMessageDB(msg);
+        }
+        //now, on envoit le nouveau message à tous les clients
+        while(clients_en_attente.length>0){
+            var client=clients_en_attente.pop(); // on enleve de la liste
+            client.writeHead(200, {'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*" });
+            client.end(JSON.stringify({			 // on lui envoie le message
+                count:list_messages.length,
+                append:req.query.message
+            }));
+        }
+        rep.writeHead(200, {'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*" });
+        rep.write(JSON.stringify(msg)); //faut absolument le garder pour que ça marche
+        rep.end();
+ });
+/** ---------------------------------------------------------------------------------------------------------
+ * ---------------------- QUAND ON APPELE URL(/addmessage) : localhost:port/addmessage ----------------------
+ * ----------------------------------------------------------------------------------------------------------*/
+ // L'utilisateur demande de changer de mode de communication
+ app.get('/choosemode', function(req, rep){
+        console.log("Je suis dans choosemode");
+        if(req.query.modecommunication != null){
+            console.log(" mode :  "+ req.query.modecommunication);
+            switch(req.query.modecommunication ){
+                case "polling":
+                    console.log("Mode polling choisi");
+                    //prevenir le client que le mode est polling
+                    //confirmMode("polling");
+                    break;
+                case "long-polling":
+                    console.log("Mode long-polling choisi");
+                    //prevenir le client que le mode est long polling
+                    //confirmMode("long-polling");
+                    break;
+                case "push":
+                    console.log("Mode push choisi");
+                    //prvenir le client que le mode est push
+                    //confirmMode("push");
+                    break;
+            }
+        }
+        //confirmer à l'utilisateur le mode choisi
+        function confirmMode(mode){
+            console.log(mode);
+            rep.writeHead(200, {'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*" });
+            rep.write(JSON.stringify(mode)); //faut absolument le garder pour que ça marche
+            rep.end();
+        }
+ });
+
+/** ----------------------------------------------------------------------------------------------------------
+ * -------------- QUAND ON APPELE URL(/longpolling) : localhost:port/  => LONG-POLLING -----------------------
+ * -----------------------------------------------------------------------------------------------------------*/
+
+ app.get('/longpolling', function(req, rep){
+    //code long polling
+    //on récupere la date d'arrivee dans le chat
+    var objet= JSON.stringify(req.query);
+    var json=JSON.parse(objet);
+    var heure=json.heure;
+    var minutes=json.minutes;
+    var secondes=json.secondes;
+    console.log("Heure : "+heure+" min: "+minutes + " sec: "+secondes);
+    //nouveau messages à envoyer
+    var new_messages=[];
+    //si le nombre de message que j'ai dans mon tableau list_message est sup à l'heure, j'envoie la data à l'user
+    if(list_messages.length>0){
+        for(var i=0; i<list_messages.length;i++){
+            console.log("le message -> : " +list_messages[i]["message"]);
+            //list_messages[i]["seconds"] > secondes ||
+            if(  list_messages[i]["hour"] > heure && list_messages[i]["minutes"] > minutes  ){
+                new_messages.push(list_messages[i]);
+            }
+        }
+    }
+    console.log("la liste des nouveau messages "+new_messages.length);
+    if(new_messages.length>0){
+        rep.writeHead(200, {'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*" });
+        rep.write(JSON.stringify(new_messages)); //faut absolument le garder pour que ça marche
+        rep.end();
+    }else{
+        clients_en_attente.push(rep);	//S'il n'y a pas de nouveau messages à envoyer au client, on l'enregistre juqu'a ce qu'il y en est de nouveaux
+        console.log("Client connecté à H: "+heure +" m: "+ minutes + "  est en attente");
+    }
+
 });
 
+
+/** ----------------------------------------------------------------------------------------------------------
+ * -------------- QUAND ON APPELE URL(/longpolling) : localhost:port/  => POLLING ----------------------------
+ * -----------------------------------------------------------------------------------------------------------*/
+app.get('/polling', function(req, rep){
+    console.log("je suis dans le polling a la seconde : ");
+    //on récupere la date d'arrivee dans le chat
+    var objet= JSON.stringify(req.query);
+    var json= JSON.parse(objet);
+    var heure= json.heure;
+    var minutes= json.minutes;
+    var secondes= json.secondes;
+    console.log("Heure : "+heure+" min: "+minutes + " sec: "+secondes);
+    // on crée un tableau pour enregistrer les nouveau messages par rapport à
+    // l'utilisateur connecté
+    var new_messages = [];
+
+    //console.log(obj.heure+' '+obj.minutes+' '+obj.secondes);
+    console.log("La liste des messages avant le IF :"+ list_messages.length);
+
+    if(list_messages.length > 0){
+        for(var i=0; i<list_messages.length;i++){
+            console.log("le message -> : " +list_messages[i]["message"]);
+            if( list_messages[i]["seconds"] > secondes ||  list_messages[i]["hour"] > heure || list_messages[i]["minutes"] > minutes  ){
+                new_messages.push(list_messages[i]);
+            }
+        }
+    }
+
+    rep.writeHead(200, {'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*" });
+    rep.write(JSON.stringify(new_messages)); //faut absolument le garder pour que ça marche
+    rep.end();
+});
+
+
+ /** ----------------------------------------------------------------------------------------------------------
+ * ----------------------- QUAND ON APPELE URL(/) : localhost:port/ -------------------------------------------
+ * -----------------------------------------------------------------------------------------------------------*/
+app.get('/', function(req, rep){
+    rep.sendFile(path.resolve('../../index.html'));
+    console.log("Je suis dans /")
+    var path = url.parse(req.url).pathname;
+    var mode_communication=req.query.modecommunication;
+});
 /** maintenant on va écouter les connections/communication qui se font
  * via notre serveur node.js à l'aide de la socket socket_io */
 
 io.sockets.on('connection', function(socket){    /** socket = socket utilisateur en cours*/
     var current_user=false;
-
     /** afficher tout les utilisateurs dans la liste*/
     for(var user in list_users){
         socket.emit('new-user-created', list_users[user]);
@@ -38,18 +179,13 @@ io.sockets.on('connection', function(socket){    /** socket = socket utilisateur
         socket.emit('new-message-coming', list_messages[msg]);
     }
 
-    /**
-     * ---------------------------------------------------------------------
-     * ---------------- GESTION MESSAGERIE---------------------------------
-     * ---------------------------------------------------------------------
-     */
+    /**---------------- GESTION MESSAGERIE -------------------- */
 
     /** Message reçu */
     socket.on('new-message-coming', function(message){
         message.user=current_user;
         date=new Date();
         message.hour=date.getHours();
-
         message.minutes=date.getMinutes();
         //sauvegarder le message
         list_messages.push(message);
@@ -61,11 +197,7 @@ io.sockets.on('connection', function(socket){    /** socket = socket utilisateur
         console.log('L id : '+message.user.id);
     });
 
-    /**
-     * ---------------------------------------------------------------------
-     * ---------------- GESTION CONNECTIONS---------------------------------
-     * ---------------------------------------------------------------------
-     */
+    /**------------- GESTION CONNECTIONS-------------------------- */
 
     /** ---LOGIN---- Dans cette partie on reçoit les évènement prevenant du côté client  **/
     socket.on('login', function(user){  // A la réception de loginEvent, j'envoi une fonction de CallBack
@@ -99,9 +231,33 @@ io.sockets.on('connection', function(socket){    /** socket = socket utilisateur
     });
 });
 
-/** ecoute des sockets sur le port 3000 du serveur */
-http_server.listen(3000, function(){
-    console.log("Listening on : 3000");
+/** ----------------------------------------------------------------------------------------------------------
+ * -------------------------------------------- FONCTIONS COMMUNES -------------------------------------------
+ * -----------------------------------------------------------------------------------------------------------*/
+// enregistrer un message dans la liste de message
+function saveMessageDB(message){
+    date=new Date();
+    var currentmessage=new Object();
+    currentmessage['message']=message;
+    currentmessage['hour']=date.getHours();
+    currentmessage['minutes']=date.getMinutes();
+    currentmessage['seconds']=date.getSeconds();
+    //enregistrer dans le tableau
+    list_messages.push(currentmessage);
+    console.log(" *** Message saved : "+currentmessage.message);
+
+};
+
+// deconnecter un utilisateur
+function deconect(){};
+
+/**  */
+
+/** ----------------------------------------------------------------------------------------------------------
+ * ----------------------------ECOUTE DES SOCKETS SUR LE PORT 3000 DU SERVEUR ---------------------------------
+ * -----------------------------------------------------------------------------------------------------------*/
+http_server.listen(port, function(){
+    console.log("Listening on : "+port);
 });
 
 
